2038b1c4c91f01b09cd74ca92b60f18b59e055f8
klimperis <lvl2111@columbia.edu>
2023-08-30 18:18:59 -0400

    loop for running cellpose on yeast tiff files updated again

--- process_yeast_cellpose.py
+++ process_yeast_cellpose.py
@@ -1,24 +1,46 @@
 import numpy as np
 import time, os, sys
 from cellpose import models, utils, io
+from run_cellpose import run_cellpose
+
+def load_data(directory):
+    ''' Load all of the images in the directory into a np array.
+    Args:
+        directory: The folder containing the images.
+    
+    Returns:
+        images (list): A sorted list of the images in the directory
+    '''
+
+    images = cellpose.io.get_image_files(directory)
+
+    #for file in os.listdir(directory):
+    #    if file.endswith('.tiff'):
+            
+    return images
 
 
-## function for running for loop on yeast (process_yeast_cellpose.py)
-def yeast_cellpose(file, path,
+## function for running for loop on yeast
+def run_cellpose_yeast(file, directory,
                    gpu='False', channel=None, 
                    model='cyto', channel_axis=None):
+    '''create masks from tiff files
+
+    Args:
+        file (str) = a .tiff file
+        directory (str) = path to file
+
+    Returns:
+        mask (np.array) = a segmented mask object for the file
     '''
-    ARGS:
-        file = a .tiff file
-        path = path to file
-        
-    RETURNS:
-        mask = a segmented mask object for the file
-    '''
+
     # calls load to tiff
-    image = load_tiff(file)
+    images = load_data(directory)
 
     # calls run_cellpose
-    mask = run_cellpose(image, gpu='False', channel=None, model='cyto', channel_axis=None)
+    masks = []
+    for image in images:
+        mask = run_cellpose(image, gpu='False', channel=None, model='cyto', channel_axis=None)
+        masks.append(mask)
 
-    return mask
+    return masks
