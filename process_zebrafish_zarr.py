06f1d70d0ad7573de1bd5f0e6b81fc5e1d1c6b4f
klimperis <lvl2111@columbia.edu>
2023-09-03 18:17:53 +0000

    removed unnecessary files, images of flow

--- process_zebrafish_zarr.py
+++ process_zebrafish_zarr.py
@@ -18,17 +18,17 @@ def process_zebrafish_zarr(zarr_file, input_group_name, output_group_name, gpu=F
 
     Args:
         zarr_file (_type_): Contains a 3D + t zebrafish data. Assume dimensions are
-            (t, z, y, x). 
+            (t, z, y, x).
         input_group_name (str): usually "gfp"
         output_group_name (str): zarr group to save the output masks to
         gpu (bool): True if you want to use gpu. defaults to False
     """
-    with zarr.open(zarr_file, 'r+') as zarr_root:
+    with zarr.open(zarr_file, "r+") as zarr_root:
         input_data = zarr_root[input_group_name]  # 4d array
         zarr_root.create_dataset(output_group_name, shape=input_data.shape)
         for frame in range(input_data.shape[0]):
             for z in range(input_data.shape[1]):
-                frame_mask = run_cellpose(input_data[frame, z], model='nuclei')
+                frame_mask = run_cellpose(input_data[frame, z], model="nuclei")
                 zarr_root[output_group_name][frame, z] = frame_mask
 
 
@@ -43,16 +43,16 @@ def process_bette_zarr(zarr_file):
         gpu (bool): True if you want to use gpu. defaults to False
     """
 
-    input_data = zarr.open(zarr_file, 'r') # 6d array
-    print('input data shape = ',input_data.shape)
-    processed_zarr_name = zarr_file.split('.zarr')[0] + '_processed' + '.zarr'
-    print('new zarr name = ',processed_zarr_name)
-    useful_data = zarr.open(processed_zarr_name,'w')
-    useful_data.create_group('exp0')
-    useful_data['exp0'] = input_data[:,0]
-    useful_data.create_group('exp1')
-    useful_data['exp1'] = input_data[:,1]
-    print('reshaped 6dimensional zarr')
+    input_data = zarr.open(zarr_file, "r")  # 6d array
+    print("input data shape = ", input_data.shape)
+    processed_zarr_name = zarr_file.split(".zarr")[0] + "_processed" + ".zarr"
+    print("new zarr name = ", processed_zarr_name)
+    useful_data = zarr.open(processed_zarr_name, "w")
+    useful_data.create_group("exp0")
+    useful_data["exp0"] = input_data[:, 0]
+    useful_data.create_group("exp1")
+    useful_data["exp1"] = input_data[:, 1]
+    print("reshaped 6dimensional zarr")
     return
 
     # # useful_data = input_data[:,1,:,1]  # use experiment 1, channel 1 -> 4D array
@@ -65,25 +65,26 @@ def process_bette_zarr(zarr_file):
 
 
 def test_zebrafish_zarr_processing():
-    # the zarr has two channels in different groups. 
+    # the zarr has two channels in different groups.
     # Each channel is (t, z, y, x)
 
     # make fake data that looks like the real data in shape and dtype but has random shapes
     time_frames = 2
-    
-    test_data = np.stack([binary_blobs(50, n_dim=3).astype(np.uint16) * 255 for _ in range(time_frames)])
-    
+
+    test_data = np.stack(
+        [binary_blobs(50, n_dim=3).astype(np.uint16) * 255 for _ in range(time_frames)]
+    )
+
     logger.debug(test_data.max())
     test_data[0] = gaussian_filter(test_data[0], sigma=1)
     test_data[1] = gaussian_filter(test_data[1], sigma=1)
     logger.debug(test_data.max())
 
-
     # save that data in a zarr group
-    zarr_name = 'fake_zebrafish_data.zarr'
-    with zarr.open(zarr_name, 'w') as zarr_file:
-        zarr_file.create_dataset('raw', data=test_data, dtype=np.uint16)
+    zarr_name = "fake_zebrafish_data.zarr"
+    with zarr.open(zarr_name, "w") as zarr_file:
+        zarr_file.create_dataset("raw", data=test_data, dtype=np.uint16)
 
-    # run run_cellpose on that zarr 
-    process_zebrafish_zarr(zarr_name, 'raw', 'mask')
+    # run run_cellpose on that zarr
+    process_zebrafish_zarr(zarr_name, "raw", "mask")
     assert 5 == 5
